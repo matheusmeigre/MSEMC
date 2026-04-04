460d3f660bf110d616d2161f66b7806c16680cb9
Matheus Meigre <matheusmeigre@hotmail.com>
2026-04-04 12:13:05 -0300

    feat: enriquecimento da classe RequisicaoEmail

--- Models/RequisicaoEmail.cs
+++ Models/RequisicaoEmail.cs
@@ -1,9 +1,23 @@
-﻿namespace MSEMC.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MSEMC.Models
 {
     public class RequisicaoEmail
     {
+        [Required]
+        [EmailAddress]
         public string Destinatario { get; set; }
+
+        [Required]
         public string Assunto { get; set; }
+
+        [Required]
         public string Conteudo { get; set; }
+
+        public List<string> Destinatarios { get; set; }
+        public List<string>? Cc { get; set; }
+        public List<string>? Bcc { get; set; }
+
+        public List<Object>? Attachments { get; set; }
     }
 }
